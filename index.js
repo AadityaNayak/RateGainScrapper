83cad595fd74c597d7087a74018259d473400470
AadityaNayak <aadityanayak7@gmail.com>
2023-11-19 01:37:54 +0530

    Corrected the date format

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const axios = require('axios');
-const cheerio = require('cheerio');
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
 const cors = require("cors");
 
 const app = express();
@@ -10,124 +10,150 @@ app.use(express.json());
 app.use(cors());
 const base_url = "https://rategain.com/blog";
 
-async function scrapeBlogData() {
-    let blogData = [];
-    let pageNum = 1;
+function extractAndFormatDate(text) {
+  // Regular expression to match date in "Month DD, YYYY" format
+  const datePattern =
+    /(January|February|March|April|May|June|July|August|September|October|November|December)\s\d{1,2},\s\d{4}/;
+  const match = text.match(datePattern);
 
-    while (true) {
-        console.log(pageNum);
-        let url = `${base_url}/page/${pageNum}/`;
-        if (pageNum === 1) url = base_url;
+  if (match) {
+    // Create a new Date object from the matched date
+    const date = new Date(match[0]);
 
-        const response = await axios.get(url, {
-            headers: {
-                "Content-Type": "application/json",
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-            }
-        });
+    // Format the date in "YYYY-MM-DD" format
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are 0-indexed
+    const day = String(date.getDate()).padStart(2, "0");
 
-        if (response.status === 200) {
-            const root = cheerio.load(response.data);
-            const articles = root("article.blog-item");
-
-            if (articles.length === 0) break;
-
-            articles.each((index, element) => {
-                const blogDetails = {};
-
-                const titleElement = root(element).find("h6");
-                blogDetails.title = titleElement.text().trim();
-
-                const dateElement = root(element).find("div.bd-item span");
-                blogDetails.date = dateElement.text().trim();
-
-                const imageElement = root(element).find("a.rocket-lazyload");
-                blogDetails.imageUrl = imageElement.attr("data-bg");
-
-                const likesElement = root(element).find("a.zilla-likes span");
-                blogDetails.likes = parseInt(likesElement.text().replace(' likes', ''));
-
-                blogData.push({
-                    "page number": pageNum,
-                    "blog title": blogDetails.title,
-                    "blog date": blogDetails.date,
-                    "image URL": blogDetails.imageUrl,
-                    "likes count": blogDetails.likes
-                });
-            });
-
-            pageNum++;
-        } else {
-            break;
-        }
-    }
-
-    return blogData;
+    return `${year}-${month}-${day}`;
+  } else {
+    return "No date found in the text";
+  }
 }
 
-async function scrapePageData(pageNum) {
-    let blogData = [];
+async function scrapeBlogData() {
+  let blogData = [];
+  let pageNum = 1;
+
+  while (true) {
+    console.log(pageNum);
     let url = `${base_url}/page/${pageNum}/`;
     if (pageNum === 1) url = base_url;
 
     const response = await axios.get(url, {
-        headers: {
-            "Content-Type": "application/json",
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-        }
+      headers: {
+        "Content-Type": "application/json",
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+      },
     });
 
     if (response.status === 200) {
-        const root = cheerio.load(response.data);
-        const articles = root("article.blog-item");
+      const root = cheerio.load(response.data);
+      const articles = root("article.blog-item");
 
-        articles.each((index, element) => {
-            const blogDetails = {};
+      if (articles.length === 0) break;
 
-            const titleElement = root(element).find("h6");
-            blogDetails.title = titleElement.text().trim();
+      articles.each((index, element) => {
+        const blogDetails = {};
 
-            const dateElement = root(element).find("div.bd-item span");
-            blogDetails.date = dateElement.text().trim();
+        const titleElement = root(element).find("h6");
+        blogDetails.title = titleElement.text().trim();
 
-            const imageElement = root(element).find("a.rocket-lazyload");
-            blogDetails.imageUrl = imageElement.attr("data-bg");
+        const dateElement = root(element).find("div.bd-item span");
+        blogDetails.date = dateElement.text().trim();
+        blogDetails.date = extractAndFormatDate(blogDetails.date);
 
-            const likesElement = root(element).find("a.zilla-likes span");
-            blogDetails.likes = parseInt(likesElement.text().replace(' likes', ''));
+        const imageElement = root(element).find("a.rocket-lazyload");
+        blogDetails.imageUrl = imageElement.attr("data-bg");
 
-            blogData.push({
-                "page number": pageNum,
-                "blog title": blogDetails.title,
-                "blog date": blogDetails.date,
-                "image URL": blogDetails.imageUrl,
-                "likes count": blogDetails.likes
-            });
+        const likesElement = root(element).find("a.zilla-likes span");
+        blogDetails.likes = parseInt(likesElement.text().replace(" likes", ""));
+
+        blogData.push({
+          "page number": pageNum,
+          "blog title": blogDetails.title,
+          "blog date": blogDetails.date,
+          "image URL": blogDetails.imageUrl,
+          "likes count": blogDetails.likes,
         });
-    }
+      });
 
-    return blogData;
+      pageNum++;
+    } else {
+      break;
+    }
+  }
+
+  return blogData;
 }
 
+async function scrapePageData(pageNum) {
+  let blogData = [];
+  let url = `${base_url}/page/${pageNum}/`;
+  if (pageNum === 1) url = base_url;
 
-app.get('/', (req, res) => {
-    res.send('Server is up and running');
+  const response = await axios.get(url, {
+    headers: {
+      "Content-Type": "application/json",
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+    },
+  });
+
+  if (response.status === 200) {
+    const root = cheerio.load(response.data);
+    const articles = root("article.blog-item");
+
+    articles.each((index, element) => {
+      const blogDetails = {};
+
+      const titleElement = root(element).find("h6");
+      blogDetails.title = titleElement.text().trim();
+
+      const dateElement = root(element).find("div.bd-item span");
+      console.log(dateElement.text());
+      blogDetails.date = dateElement.text().trim();
+      blogDetails.date = extractAndFormatDate(blogDetails.date);
+
+
+      const imageElement = root(element).find("a.rocket-lazyload");
+      blogDetails.imageUrl = imageElement.attr("data-bg");
+
+      const likesElement = root(element).find("a.zilla-likes span");
+      blogDetails.likes = parseInt(likesElement.text().replace(" likes", ""));
+
+      blogData.push({
+        "page number": pageNum,
+        "blog title": blogDetails.title,
+        "blog date": blogDetails.date,
+        "image URL": blogDetails.imageUrl,
+        "likes count": blogDetails.likes,
+      });
+    });
+  }
+
+  return blogData;
+}
+
+app.get("/", (req, res) => {
+  res.send("Server is up and running");
 });
 
-app.get('/all_pages', async (req, res) => {
-    const blogData = await scrapeBlogData();
-    console.log(blogData);
-    res.json(blogData);
+app.get("/all_pages", async (req, res) => {
+  const blogData = await scrapeBlogData();
+//   console.log(blogData);
+  res.json(blogData);
 });
 
-app.get('/page/:page_number', async (req, res) => {
-    const pageNum = req.params.page_number;
-    const blogData = await scrapePageData(pageNum);
-    console.log(blogData);
-    if(blogData.length === 0) res.send("Page is empty or does not exist")
-    res.json(blogData);
+app.get("/page/:page_number", async (req, res) => {
+  const pageNum = req.params.page_number;
+  const blogData = await scrapePageData(pageNum);
+//   console.log(blogData);
+  if (blogData.length === 0) res.send("Page is empty or does not exist");
+  res.json(blogData);
 });
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
