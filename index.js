c3ee5dee7b46717faddf8e4594bcce8561e2544c
AadityaNayak <aadityanayak7@gmail.com>
2023-11-20 21:41:52 +0530

    Experiment

--- index.js
+++ index.js
@@ -152,6 +152,6 @@ app.get("/page/:page_number", async (req, res) => {
   res.json(blogData);
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+// app.listen(3000, () => {
+//   console.log("Server is running on port 3000");
+// });
